b5db04fe1000c5dd1820098b8bc115da84f7e54e
Tyler Ruggles <tylerhruggles@gmail.com>
2019-11-20 14:33:35 -0800

    Larger range

--- mazama_submit.sh
+++ mazama_submit.sh
@@ -5,10 +5,10 @@
 
 
 export DATE=20191120
-export VERSION=v4
+export VERSION=v5
 
 for reliability in 0.9997; do
-    for wind in 0.0 0.25 0.5 0.75 1.0 1.25 1.5 1.75 2.0; do
+    for wind in 0.0 0.25 0.5 0.75 1.0 1.25 1.5 1.75 2.0 2.25 2.5 2.75 3.0 3.5 4.0 4.5 5.0; do
 
         export RELIABILITY=$reliability
         export WIND=$wind
