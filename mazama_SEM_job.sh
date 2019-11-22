36d1dd4c6356d33e52757888bbd63014427811a5
Tyler Ruggles <tylerhruggles@gmail.com>
2019-11-21 23:10:52 -0800

    zero storage, clunky

--- mazama_SEM_job.sh
+++ mazama_SEM_job.sh
@@ -13,5 +13,6 @@ cd $PBS_O_WORKDIR
 module load anaconda/anaconda3
 module load gurobi752
 python run_reliability_analysis.py "date_$DATE" "version_$VERSION" "reliability_$RELIABILITY" "wind_$WIND" "run_sem"
+# "zero_storage" # <-- Can grab this for zero storage runs
 # end script
 
