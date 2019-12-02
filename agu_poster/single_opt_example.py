8baeba8124c94298b9baffae6acb775d8adf2c06
Tyler Ruggles <truggles@wisc.edu>
2019-12-01 21:27:51 -0800

    Adding another reliability value

--- agu_poster/single_opt_example.py
+++ agu_poster/single_opt_example.py
@@ -5,6 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
+save_type = 'png'
+save_type = 'pdf'
+
+us_mean_dem = 450 # GW
 
 # Create the 1) initial optimization df and 2) the results df
 def split_df(df):
@@ -26,32 +30,36 @@ def split_df(df):
     return df_master, df_results
 
 
-def plot_caps(df):
+def plot_caps(df, unmet):
 
     plt.close()
     fig, ax = plt.subplots()
 
     for i, idx in enumerate(df.index):
-        info = [df.loc[idx, 'capacity wind (kW)'],
+        info = np.array([df.loc[idx, 'capacity wind (kW)'],
                 df.loc[idx, 'capacity solar (kW)'],
                 df.loc[idx, 'capacity nuclear (kW)'],
-                df.loc[idx, 'capacity storage (kW)']]
-        ax.plot(range(4), info, markersize=10, marker='o', linestyle='dashed', label=f'Year {i+1}')
+                df.loc[idx, 'capacity storage (kW)']])
+        ax.plot(range(4), info*us_mean_dem, markersize=10, marker='o', linestyle='dashed', label=f'Year {i+1}')
     ax.set_ylim(0, ax.get_ylim()[1])
-    ax.set_ylabel('Capacity (kW or kWh)')
-    plt.xticks(range(4), ['Wind (kW)', 'Solar (kW)', 'Nuclear (kW)', 'Storage (kWh)'])
+    ax.set_ylabel('Nameplate Capacity (GW or GWh)')
+    plt.xticks(range(4), ['Wind (GW)', 'Solar (GW)', 'Nuclear (GW)', 'Storage (GWh)'])
     plt.legend(loc='lower right')
-    plt.savefig('plots/single_cap.png')
+    plt.tight_layout()
+    plt.savefig('plots/single_cap_'+str(unmet).replace('.','p')+'.'+save_type)
 
 
-def plot_unmet(df, unmet_target):
+def plot_unmet(df, unmet):
 
     plt.close()
     fig, ax = plt.subplots()
 
     years = OrderedDict()
+    max_v = 0.
     for idx in df.index:
         info = df.loc[idx, 'case name'].split('_')
+        if df.loc[idx, 'dispatch unmet demand (kW)'] > max_v:
+            max_v = df.loc[idx, 'dispatch unmet demand (kW)']
         if info[-2] in years.keys(): # This checks the leading (master) year
             years[info[-2]].append(df.loc[idx, 'dispatch unmet demand (kW)'])
         else:
@@ -64,21 +72,40 @@ def plot_unmet(df, unmet_target):
         i += 1
 
 
-    ax.plot(np.linspace(-1, len(years), 100), np.ones(100)*unmet_target, 'k-', label='Target Unmet\nDemand')
+    ax.plot(np.linspace(-1, len(years), 100), np.ones(100)*unmet, 'k-', label='Target Unmet\nDemand')
 
     #ax.set_ylim(0, ax.get_ylim()[1])
-    ax.set_ylim(0, unmet_target*4)
+    ax.set_ylim(0, max_v*1.3)
     ax.set_xlim(-0.25, 0.25+len(years)-1)
-    ax.set_ylabel('Unmet Demand (kW)')
-    plt.xticks(range(4), [f'Year {i+1}\nOptimization' for i in range(4)])
+    ax.set_ylabel('Fraction of Unmet Demand')
+    plt.xticks(range(4), [f'Year {i+1}\nConfiguration' for i in range(4)])
     plt.legend(loc='upper right')
-    plt.savefig('plots/single_unmet.png')
+    plt.tight_layout()
+    plt.savefig('plots/single_unmet_'+str(unmet).replace('.','p')+'.'+save_type)
 
-df = pd.read_csv('Results_reliability_20191201_v1_wind.csv', index_col=False)
-master, results = split_df(df)
+def print_variance(df):
+        info = ['capacity wind (kW)', 'capacity solar (kW)',
+                'capacity nuclear (kW)', 'capacity storage (kW)']
 
-plot_caps(master)
-plot_unmet(results, 0.001)
+        for col in df.columns:
+            if col not in info:
+                continue
+            print(col, round(np.std(df[col]),4), round(np.std(df[col])/np.mean(df[col]),4), round((max(df[col]) - min(df[col]))/np.mean(df[col]),4))
+    
+
+file_map = { # Results file : associated unmet demand
+    'Results_reliability_20191201_v1_wind.csv' : 0.001,
+    'Results_reliability_20191201_v2_wind.csv' : 0.0003,
+}
+
+for f, unmet in file_map.items():
+    print(unmet, f)
+    df = pd.read_csv(f, index_col=False)
+    master, results = split_df(df)
+    
+    plot_caps(master, unmet)
+    plot_unmet(results, unmet)
+    print_variance(master)
 
 
 
