81d3d1c82515cdcff58f153eaa3a4b873ee7c2e2
Tyler Ruggles <tylerhruggles@gmail.com>
2019-12-18 11:47:20 -0800

    Look like we made it past the 2 hr mark on mazama

--- mazama_SEM_job.sh
+++ mazama_SEM_job.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 #SBATCH --job-name=reli_${DATE}_${VERSION}_${RELIABILITY}_${WIND}
+#
+#SBATCH --partition=clab
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4G
@@ -11,4 +13,3 @@ module load anaconda/anaconda3
 module load gurobi752
 srun python run_reliability_analysis.py "date_$DATE" "version_$VERSION" "reliability_$RELIABILITY" "wind_$WIND" "run_sem" $EXTRA_ARGS
 # end script
-#SBATCH --time=0-15 # 0days-15hours
