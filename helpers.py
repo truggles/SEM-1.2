a777f3d30183028245d76b8c80f582459dae892e
Tyler Ruggles <truggles@wisc.edu>
2020-02-26 22:03:22 -0800

    A reasonable 1st version plot of dispatch

--- helpers.py
+++ helpers.py
@@ -5,6 +5,7 @@ import os
 from glob import glob
 import pandas as pd
 import matplotlib
+import matplotlib.pyplot as plt
 from datetime import datetime
 
 
@@ -13,36 +14,35 @@ def plot_peak_demand_system(out_file_name, save_dir, save_name):
 
     # Open out file as df
     df = pd.read_csv(out_file_name)
-    print(df.head())
 
     # Find the peak hour
     peak_idxs = df[ df['demand (kW)'] == np.max(df['demand (kW)'])].index
     assert(len(peak_idxs) == 1), f"\n\nThere are multiple instances of peak demand value, {peak_idxs}\n\n"
     peak_idx = peak_idxs[0]
-    print(peak_idx)
-    print(peak_idx, df.iloc[peak_idx-3:peak_idx+3])
-
-    return
+    max_demand = np.max(df['demand (kW)'])
 
     plt.close()
-    matplotlib.rcParams["figure.figsize"] = (6.4, 4.8)
+    matplotlib.rcParams["figure.figsize"] = (6, 5)
     fig, ax = plt.subplots()
-    ax.set_xlabel(kwargs['x_label'])
-    ax.set_ylabel(kwargs['y_label'])
-    plt.title(kwargs['title'])
+    ax.set_xlabel('Hours')
+    ax.set_ylabel('Power (kW)')
+    lo = peak_idx - 24*4
+    hi = peak_idx + 24*3
+    dfs = df.iloc[lo:hi]
+    xs = dfs['time (hr)']
+    cap_nuke = np.max(df['dispatch nuclear (kW)'])
 
-    ax.fill_between(kwargs['x_vals'], 0., kwargs['nuclear'], color='red', label=f'nuclear {kwargs["legend_app"]}')
-    if 'renewables' in kwargs.keys():
-        ax.fill_between(kwargs['x_vals'], kwargs['nuclear'], kwargs['nuclear']+kwargs['renewables'], color='green', label=f'renewables {kwargs["legend_app"]}')
-    else:
-        ax.fill_between(kwargs['x_vals'], kwargs['nuclear'], kwargs['nuclear']+kwargs['wind'], color='blue', label=f'wind {kwargs["legend_app"]}')
-        ax.fill_between(kwargs['x_vals'], kwargs['nuclear']+kwargs['wind'], kwargs['nuclear']+kwargs['wind']+kwargs['solar'], color='yellow', label=f'solar {kwargs["legend_app"]}')
+    ax.fill_between(xs, 0., dfs['dispatch nuclear (kW)'], color='red', alpha=0.2, label='Nuclear Dispatch')
+    ax.fill_between(xs, dfs['dispatch nuclear (kW)'] - dfs['dispatch to fuel h2 storage (kW)'], dfs['dispatch nuclear (kW)'], facecolor='none', edgecolor='blue', hatch='xxx', label='Electrolyzer Consumption')
 
+    ax.plot(xs, dfs['demand (kW)'], 'k-', linewidth=2, label='Demand')
+    ax.plot(xs, np.ones(len(xs))*cap_nuke, 'r-', linewidth=1, label='Capacity Nuclear')
 
+    ax.set_ylim(0, max_demand*1.5)
+    ax.set_xlim(lo, hi)
     plt.tight_layout()
-    plt.legend()
-    plt.grid()
-    fig.savefig(f'{save_dir}/{save_name}.png')
+    plt.legend(ncol=2)
+    fig.savefig(f'{save_dir}/{save_name}.pdf')
 
 
 
