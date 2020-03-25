df737bb75ca2b9ba268e10908dedc3a67a214d12
Tyler Ruggles <tylerhruggles@gmail.com>
2020-03-25 13:47:57 -0700

    Add Alaska and Hawaii to geo plots

--- regional_analysis/regional_comparison_plotting.py
+++ regional_analysis/regional_comparison_plotting.py
@@ -6,8 +6,10 @@ from scipy import stats
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import cartopy
 import cartopy.crs as ccrs
 import cartopy.io.shapereader as shpreader
+import shapely.geometry as sgeom
 
 import analytic_fuels as af
 
@@ -28,6 +30,33 @@ ice_mpgs = [30, 40,] # ICE / HICE MPG
 
 def marker_list():
     return ['o', 'v', '^', 's', 'P', '*', 'H', 'X', 'd', '<', '>']
+
+# How to add Alaska and Hawaii to cartopy map
+# From: https://stackoverflow.com/questions/55598249/showing-alaska-and-hawaii-in-cartopy-map
+def add_insetmap(axes_extent, map_extent, state_name, facecolor, edgecolor, geometry):
+    # create new axes, set its projection
+    use_projection = ccrs.Mercator()     # preserve shape well
+    #use_projection = ccrs.PlateCarree()   # large distortion in E-W for Alaska
+    geodetic = ccrs.Geodetic(globe=ccrs.Globe(datum='WGS84'))
+    sub_ax = plt.axes(axes_extent, projection=use_projection)  # normal units
+    sub_ax.set_extent(map_extent, geodetic)  # map extents
+
+    # add basic land, coastlines of the map
+    # you may comment out if you don't need them
+    sub_ax.add_feature(cartopy.feature.LAND)
+    sub_ax.coastlines()
+
+    sub_ax.set_title(state_name)
+
+    # add map `geometry` here
+    sub_ax.add_geometries([geometry], ccrs.PlateCarree(), \
+                          facecolor=facecolor, edgecolor=edgecolor)
+    # +++ more features can be added here +++
+
+    # plot box around the map
+    extent_box = sgeom.box(map_extent[0], map_extent[2], map_extent[1], map_extent[3])
+    sub_ax.add_geometries([extent_box], ccrs.PlateCarree(), color='none', linewidth=0.05)
+
 
 
 
@@ -52,6 +81,11 @@ def plot_fuel_econ(df, var, axis_label, **kwargs):
     
     ax.background_patch.set_visible(False)
     ax.outline_patch.set_visible(False)
+
+    if 'text' in kwargs:
+        plt.text( np.mean([lat_1, lat_2]), lon_2*1.025, kwargs['text'],
+            horizontalalignment='center',
+            transform=ccrs.Geodetic())
 
 
     # https://stackoverflow.com/questions/25408393/getting-individual-colors-from-a-color-map-in-matplotlib
@@ -118,18 +152,35 @@ def plot_fuel_econ(df, var, axis_label, **kwargs):
             #if prices_state == 'Washington':
             print(f"{astate.attributes['name']:>30} {round(val,4)} {var}")
         
+        # special handling for the 2 states
+        # ---------------------------------
+        if geo_state in ["Alaska", "Hawaii"]:
 
-        
-        # `astate.geometry` is the polygon to plot
-        ax.add_geometries([astate.geometry], ccrs.PlateCarree(),
+            # prep map settings
+            # experiment with the numbers in both `_extents` for your best results
+            if geo_state == "Alaska":
+                # (1) Alaska
+                map_extent = (-178, -135, 46, 73)    # degrees: (lonmin,lonmax,latmin,latmax)
+                axes_extent = (0.04, 0.06, 0.29, 0.275) # axes units: 0 to 1, (LLx,LLy,width,height)
+
+            if geo_state == "Hawaii":
+                # (2) Hawii
+                map_extent = (-162, -152, 15, 25)
+                axes_extent = (0.27, 0.06, 0.15, 0.15)
+
+            # add inset maps
+            add_insetmap(axes_extent, map_extent, geo_state, \
+                         facecolor, \
+                         edgecolor, \
+                         astate.geometry)
+
+        # the other (conterminous) states go here
+        else:
+            # `astate.geometry` is the polygon to plot
+            ax.add_geometries([astate.geometry], ccrs.PlateCarree(),
                           facecolor=facecolor, edgecolor=edgecolor)
 
-    if 'text' in kwargs:
-        plt.text( np.mean([lat_1, lat_2]), lon_2*1.025, kwargs['text'],
-            horizontalalignment='center',
-            transform=ccrs.Geodetic())
-
-    cax = fig.add_axes([.2, .12, .6, 0.04]) # Start X, start Y, X width, Y width
+    cax = fig.add_axes([.45, .12, .4, 0.04]) # Start X, start Y, X width, Y width
     if 'break-even' in kwargs and kwargs['break-even']:
         extend = 'max'
     else:
