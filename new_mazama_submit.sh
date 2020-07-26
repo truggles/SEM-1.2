89b3e6de6e45ea3d0175b84b9cc248d34f64ef8d
Tyler Ruggles <tylerhruggles@gmail.com>
2020-07-25 21:35:31 -0700

    Latest submission

--- new_mazama_submit.sh
+++ new_mazama_submit.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 
 
-export DATE=20200608
+export DATE=20200725
 export VERSION=v1
-nJobs=25
+nJobs=26
 
         #"Case0_NuclearFlatDemand" \
-        #"Case6_NuclearWindSolarStorage" \
+
+        #"Case1_Nuclear" \
+        #"Case2_NuclearStorage" \
+        #"Case3_WindStorage" \
+        #"Case4_SolarStorage" \
+        #"Case5_WindSolarStorage" \
 
 
 for CASE in \
-        "Case1_Nuclear" \
-        "Case2_NuclearStorage" \
-        "Case3_WindStorage" \
-        "Case4_SolarStorage" \
-        "Case5_WindSolarStorage" \
+        "Case6_NuclearWindSolarStorage" \
         ; do
 
     for (( JOB=1; JOB<=$nJobs; JOB++ )); do
 
         echo "${CASE} nJobs_${nJobs} jobNum_${JOB}"
-        export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR factor_1.1"
+        export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR"
         export SBATCH_JOB_NAME=test_fuel_${DATE}_${VERSION}_${CASE}_${nJobs}_${JOB} 
         sbatch new_mazama_SEM_job.sh
 
