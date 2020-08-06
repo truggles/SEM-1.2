4724d66722a90905d7e51e7042c693434eeca16a
Tyler Ruggles <tylerhruggles@gmail.com>
2020-08-05 21:25:42 -0700

    New submissions

--- new_mazama_submit.sh
+++ new_mazama_submit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-export DATE=20200803
-export VERSION=v2
+export DATE=20200805
+export VERSION=v5
 nJobs=103
 
         #"Case0_NuclearFlatDemand" \
@@ -10,20 +10,21 @@ nJobs=103
         #"Case2_NuclearStorage" \
         #"Case3_WindStorage" \
         #"Case4_SolarStorage" \
-        #"Case5_WindSolarStorage" \
         #"Case1_Nuclear" \
         #"Case6_NuclearWindSolarStorage" \
+        #"Case8_NatGasCCSStorage" \ # not interesting by default b/c zero storage is built
 
 for CASE in \
+        "Case5_WindSolarStorage" \
         "Case7_NatGasCCS" \
-        "Case8_NatGasCCSStorage" \
         "Case9_NatGasCCSWindSolarStorage" \
         ; do
 
     for (( JOB=1; JOB<=$nJobs; JOB++ )); do
 
         echo "${CASE} nJobs_${nJobs} jobNum_${JOB}"
-        export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR"
+        export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR H2_ONLY"
+        #export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR"
         export SBATCH_JOB_NAME=test_fuel_${DATE}_${VERSION}_${CASE}_${nJobs}_${JOB} 
         sbatch new_mazama_SEM_job.sh
 
