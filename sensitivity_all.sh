7da2197e5f4520057853e9ef21edd18f7cf12065
Tyler Ruggles <tylerhruggles@gmail.com>
2020-11-16 07:30:00 -0800

    Final reruns of sensitivity analysis

--- sensitivity_all.sh
+++ sensitivity_all.sh
@@ -12,25 +12,25 @@ version="v2"
 for CASE in \
         "Case7_NatGasCCS" \
         ; do
-    for APP in "EL0.5" "EL0.75" "NG1.0" "NG0.75" "NG0.5"; do
+    for APP in ""; do # "EL0.5" "EL0.75" "NG1.0" "NG0.75" "NG0.5"; do
         ./run_SEM_configs_fuels.py "date_$Date" $CASE "version_${version}${APP}" $ARGS H2_ONLY
     done
 done
 
-version="v3"
-for CASE in \
-        "Case5_WindSolarStorage" \
-        ; do
-    for APP in "" "EL0.5" "EL0.75" "SOL0.75" "SOL0.5" "WIND0.75" "WIND0.5"; do
-        ./run_SEM_configs_fuels.py "date_$Date" $CASE "version_${version}${APP}" $ARGS H2_ONLY
-    done
-done
+#version="v3"
+#for CASE in \
+#        "Case5_WindSolarStorage" \
+#        ; do
+#    for APP in "" "EL0.5" "EL0.75" "SOL0.75" "SOL0.5" "WIND0.75" "WIND0.5"; do
+#        ./run_SEM_configs_fuels.py "date_$Date" $CASE "version_${version}${APP}" $ARGS H2_ONLY
+#    done
+#done
 
 version="v4"
 for CASE in \
         "Case9_NatGasCCSWindSolarStorage" \
         ; do
-    for APP in "" "EL0.5" "EL0.75" "SOL0.75" "SOL0.5" "WIND0.75" "WIND0.5" "NG0.75" "NG0.5"; do
+    for APP in ""; do # "EL0.5" "EL0.75" "SOL0.75" "SOL0.5" "WIND0.75" "WIND0.5" "NG0.75" "NG0.5"; do
         ./run_SEM_configs_fuels.py "date_$Date" $CASE "version_${version}${APP}" $ARGS H2_ONLY
     done
 done
--- sensitivity_new_mazama_submit.sh
+++ sensitivity_new_mazama_submit.sh
@@ -6,13 +6,12 @@ nJobs=103
 
 
 #export CASE="Case7_NatGasCCS"
-##for EL in 0.5; do
-##    export VERSION="v2EL${EL}"
-#for NG in 0.75; do
-#    export VERSION="v2NG${NG}"
+#for EL in 0.5; do
+#    export VERSION="v2EL${EL}"
+##for NG in 0.75; do
+#    #export VERSION="v2NG${NG}"
 ##    export VERSION="v2"
-#    #for (( JOB=1; JOB<=$nJobs; JOB++ )); do
-#    for JOB in 62; do
+#    for (( JOB=1; JOB<=$nJobs; JOB++ )); do
 #        echo "${CASE} version_${VERSION} nJobs_${nJobs} jobNum_${JOB}"
 #        #export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR H2_ONLY"
 #        #export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR H2_ONLY FIXED_NATGASCCS_${NG}"
@@ -24,10 +23,10 @@ nJobs=103
 
 
 
-#export CASE="Case5_WindSolarStorage"
+export CASE="Case5_WindSolarStorage"
 #export VERSION="v3"
-#for (( JOB=1; JOB<=$nJobs; JOB++ )); do
-#
+##for (( JOB=1; JOB<=$nJobs; JOB++ )); do
+#for JOB in 64 98 101 103; do
 #    echo "${CASE} version_${VERSION} nJobs_${nJobs} jobNum_${JOB}"
 #    export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR H2_ONLY"
 #    export SBATCH_JOB_NAME=test_fuel_${DATE}_${VERSION}_${CASE}_${nJobs}_${JOB} 
@@ -43,9 +42,10 @@ nJobs=103
 #        sbatch new_mazama_SEM_job.sh
 #    done
 #done
-#for SOL in 0.75 0.5; do
+#for SOL in 0.75; do
 #    export VERSION="v3SOL${SOL}"
-#    for (( JOB=1; JOB<=$nJobs; JOB++ )); do
+#    #for (( JOB=1; JOB<=$nJobs; JOB++ )); do
+#    for JOB in 63 99 100 102 103; do
 #
 #        echo "${CASE} version_${VERSION} nJobs_${nJobs} jobNum_${JOB}"
 #        export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR H2_ONLY FIXED_SOLAR_${SOL}"
@@ -70,15 +70,16 @@ nJobs=103
 
 
 
-#export CASE="Case9_NatGasCCSWindSolarStorage"
-#export VERSION="v4"
+export CASE="Case9_NatGasCCSWindSolarStorage"
+export VERSION="v4"
 #for (( JOB=1; JOB<=$nJobs; JOB++ )); do
-#
-#    echo "${CASE} version_${VERSION} nJobs_${nJobs} jobNum_${JOB}"
-#    export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR H2_ONLY"
-#    export SBATCH_JOB_NAME=test_fuel_${DATE}_${VERSION}_${CASE}_${nJobs}_${JOB} 
-#    sbatch new_mazama_SEM_job.sh
-#done
+for JOB in 17 19 25 26 27 28 29 30 31 33 34 36 37 38 39 40 41 42 43 44 45 46 47 48 49 51 52 53 54 57 58 59 60 62 63 66 69 72 76 77 87 89 91 97 98 103; do
+
+    echo "${CASE} version_${VERSION} nJobs_${nJobs} jobNum_${JOB}"
+    export EXTRA_ARGS="${CASE} nJobs_${nJobs} jobNum_${JOB} FULL_YEAR H2_ONLY"
+    export SBATCH_JOB_NAME=test_fuel_${DATE}_${VERSION}_${CASE}_${nJobs}_${JOB} 
+    sbatch new_mazama_SEM_job.sh
+done
 #sleep 600
 #for EL in 0.75 0.5; do
 #    export VERSION="v4EL${EL}"
